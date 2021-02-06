7ec94356c64d6345bdf0416942b2fbb2a521274a
Tim Havelka <tim.havelka@gmail.com>
2021-02-06 17:25:11 -0600

    Initial commit

---
+++ app/src/main/java/io/thavelka/githubbasic/MainActivity.java
@@ -0,0 +1,14 @@
+package io.thavelka.githubbasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
