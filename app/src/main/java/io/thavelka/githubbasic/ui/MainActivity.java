28f3572abb1ae4cb1f648f661072663d50e479e9
Tim Havelka <tim.havelka@gmail.com>
2021-02-07 15:40:31 -0600

    Update app title

--- app/src/main/java/io/thavelka/githubbasic/ui/MainActivity.java
+++ app/src/main/java/io/thavelka/githubbasic/ui/MainActivity.java
@@ -1,20 +1,11 @@
 package io.thavelka.githubbasic.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
-import android.util.Log;
 
-import com.google.gson.Gson;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 import io.thavelka.githubbasic.R;
-import io.thavelka.githubbasic.api.GithubService;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.title_main);
+        }
     }
 }
