31e1953205abb96c4b9ce99ce8fb0976758601b2
Tim Havelka <tim.havelka@gmail.com>
2021-02-06 18:18:01 -0600

    Initial commits fragment

---
+++ app/src/main/java/io/thavelka/githubbasic/ui/CommitsFragment.java
@@ -0,0 +1,25 @@
+package io.thavelka.githubbasic.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.thavelka.githubbasic.R;
+
+public class CommitsFragment extends Fragment {
+
+    public CommitsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_commits, container, false);
+    }
+}
